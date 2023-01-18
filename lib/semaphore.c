25b316bb2f1a26f289a63141d327d0c9b4da5938
Katherine456719 <alexandra.lung@s.unibuc.ro>
2023-01-18 21:27:35 +0200

    Added implementation for semaphore

---
+++ lib/semaphore.c
@@ -0,0 +1,73 @@
+//
+// Created by kath on 18.01.2023.
+//
+#include "semaphore.h"
+#include "thread.h"
+
+void semaphore_init(semaphore *semaphore1, int value) {
+    semaphore1->value = value;
+
+    //The list of waiting threads
+    thread_list_init(&semaphore1->wait_list);
+
+    //Initialization for the spinlock of the semaphore
+    spinlock_init(&semaphore1->lock)
+}
+
+//Free memory used by semaphore
+void semaphore_destroy(semaphore *semaphore1) {
+    semaphore1->value = 0;
+    thread_list_delete(&semaphore1->wait_list);
+    spinlock_delete(&semaphore1->lock);
+}
+
+void semaphore_wait(semaphore *semaphore1){
+    spinlock_lock(&semaphore1->lock);
+    // If the value is 0, make the thread go to sleep
+    // and add it to the list of waiting threads.
+    if(s->value==0){
+        thread * this = malloc(sizeof(struct thread));
+
+        this->info.thread_id = gettid();
+        this->next = NULL;
+
+        // Add it to the waiting list.
+        thread_list_add(&semaphore1->wait_list,this);
+
+        // Initialize the signal set.
+        sigset_t block_signals;
+        sigemptyset(&block_signals);
+        sigaddset(&block_signals, SIGUSR1);
+
+        // Block the signals.
+        sigprocmask(SIG_BLOCK, &block_signals, NULL);
+
+        // We block the signal before unlocking the
+        // spinlock to avoid a race condition.
+        spinlock_unlock(&semaphore1->lock);
+
+        // Go to sleep until we receive SIGUSR1.
+        int received;
+        sigwait(&block_signals,&received);
+    }
+    else{
+        semaphore1->value--;
+        spinlock_unlock(&semaphore1->lock);
+    }
+}
+
+void semaphore_signal(semaphore *semaphore1) {
+    spinlock_lock(&semaphore1->lock);
+
+    //The next thread that is waiting will be chosen
+    if (!thread_list_empty(&semaphore1->wait_list)) {
+        thread *next_thr = thread_list_pop(&semaphore1->wait_list);
+        spinlock_unlock(&semaphore1->lock);
+
+        tgkill(next_thr->info.thread_id, SIGUSR1);
+        free(next_thr);
+    } else {
+        semaphore1->value++;
+        spinlock_unlock(&semaphore1->lock);
+    }
+}
---
+++ lib/semaphore.h
@@ -0,0 +1,49 @@
+//
+// Created by kath on 18.01.2023.
+//
+
+#include"spinlock.h"
+#include"defines.h"
+
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+
+// A semaphore is a type of lock that works using two atomic instructions:
+// signal() will increment the value of the semaphore.
+// wait() will block the thread until the value of the semaphore is greater
+// than 0. Once the value is greater than 0, the value will be decremented
+// and the thread will be unblocked.
+
+
+
+// This structure manages a semaphore.
+// value - the value of the semaphore.
+// thread_list - the list of threads that are waiting to aquire the lock.
+// lock - a spinlock used to make wait() and signal() atomic.
+typedef struct semaphore {
+    int value;
+    thread_list wait_list;
+    spinlock lock;
+} semaphore;
+
+
+// When the value of the semaphore is 0, threads that
+// call wait() are added into the list of waiting threads.
+// When a call to signal() occurs, waiting threads are chosen
+// for execution in the FIFO (first-in-first out) order.
+
+
+// Initializes the semaphore with the given value.
+void semaphore_init(semaphore *semaphore1, int value);
+
+// Destroys the semaphore.
+void semaphore_destroy(semaphore *semaphore1);
+
+// Waits until the thread can aquire the semapore.
+void semaphore_wait(semaphore *semaphore1);
+
+// Increments the value of the semaphore.
+void semaphore_signal(semaphore *semaphore1);
+
+#endif
